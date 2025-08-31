be28485e09540d057f067d24f91664d229c1448d
jefritambunan <jefriaritambunan@gmail.com>
2025-08-31 16:05:39 +0700

    First

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var game
+var testObj
+window.onload = function() {
+  var config = {
+    type: Phaser.AUTO,
+    width: 480,
+    height: 640,
+    parent: 'phaser-game',
+    scene: [SceneMain]
+  }
+  game = new Phaser.Game(config)
+}
